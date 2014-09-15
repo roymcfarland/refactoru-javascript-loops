e838bb8bb969df008df12aea1000804c8e075cd6
Roy McFarland <roy.mcfarland@gmail.com>
2014-09-15 14:58:28 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+// Loop Practice
+
+/*
+for(var i=0; i<5; i++) {
+	console.log(i);
+}
+*/
+
+/*
+for(var i=0; i<600; i+=100) {
+	console.log(i);
+}
+*/
+
+/*
+for(var i=1; i<65; i*=2) {
+	console.log(i);
+}
+*/
+
+/*
+for(var i=1; i<4; i++) {
+	console.log(i);
+	console.log(i);
+	console.log(i);
+}
+*/
+
+/*
+for(var i=0; i<11; i+=2) {
+	console.log(i);
+}
+*/
+
+/*
+for(var i=3; i<16; i+=3) {
+	console.log(i);
+}
+*/
+
+
+for(var i=9; i>=0; i--) {
+	console.log(i);
+}
+
+
+/*
+for(var i=0; i<4; i++) {
+	console.log(i);
+}
+
+for(var i=0; i<4; i++) {
+	console.log(i);
+}
+
+for(var i=0; i<4; i++) {
+	console.log(i);
+}
+*/
